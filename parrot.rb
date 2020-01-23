ee0b504eb02c5c860ea8f4975b354e9cdc78bc05
sasha walker <sashawalker31@gmail.com>
2020-01-23 03:45:56 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,10 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+def parrot
+  puts "Squawk!"
+  return "Squawk!"
+  phrase = parrot("Pretty bird!")
+  puts phrase
+end
+
